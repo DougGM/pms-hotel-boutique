f2fa96bed6b356cdd4b9a09d2acc808e5f6578c4
Cesar <pcesarramospablo.24@gmail.com>
2026-09-11 16:58:04 -0600

    Detalle de habitación

--- src/services/roomService.ts
+++ src/services/roomService.ts
@@ -1,13 +1,25 @@
 import { toDomain as toRoom, type Room } from '@/shared/types/entities/room';
+import { toDomain as toRate, type Rate } from '@/shared/types/entities/rate';
+import { toDomain as toRoomFeature, type RoomFeature } from '@/shared/types/entities/room-feature';
 import { toDomain as toRoomType, type RoomType } from '@/shared/types/entities/room-type';
 import type { ID } from '@/shared/types/common';
-import { roomsDB, roomTypesDB } from '@/data/db';
+import { ratesDB, roomFeaturesDB, roomsDB, roomTypesDB } from '@/data/db';
 import { mockUtils, simulateLatency } from './mockUtils';
 export const roomService = {
   async getRoomTypes(): Promise<RoomType[]> {
     await simulateLatency();
     mockUtils.throwIfSimulatingError('No fue posible cargar los tipos de habitacion.');
     return roomTypesDB.map(toRoomType);
+  },
+  async getRoomFeatures(): Promise<RoomFeature[]> {
+    await simulateLatency();
+    mockUtils.throwIfSimulatingError('No fue posible cargar las caracteristicas.');
+    return roomFeaturesDB.map(toRoomFeature);
+  },
+  async getRates(): Promise<Rate[]> {
+    await simulateLatency();
+    mockUtils.throwIfSimulatingError('No fue posible cargar las tarifas.');
+    return ratesDB.map(toRate);
   },
   async getRooms(): Promise<Room[]> {
     await simulateLatency();
